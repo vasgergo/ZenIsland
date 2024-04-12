16c379e69c8abfcbfe9b5b3da79e439700c4adb8
Vas Gergő Zoltán <h166048@stud.u-szeged.hu>
2024-04-12 17:06:23 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"vasgergo-webkert","appId":"1:924022631597:web:0e09a16f5544e0edcdaa24","storageBucket":"vasgergo-webkert.appspot.com","apiKey":"AIzaSyAdeSdRm0crRW2JfUEo3BI7JAA2lzlSysE","authDomain":"vasgergo-webkert.firebaseapp.com","messagingSenderId":"924022631597","measurementId":"G-4WS8KV1PG7"})),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ],
+  providers: [
+    ScreenTrackingService,
+    UserTrackingService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
