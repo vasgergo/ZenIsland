1568e2faaa770309e4a665c1d57abb2f3130c5ed
Vas Gergő Zoltán <h166048@stud.u-szeged.hu>
2024-04-15 17:23:06 +0200

    feat: create signup and login module

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+
+  login() {
+    console.log(this.loginForm.value);
+  }
+}
---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent {
+
+  signUpForm = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.warn(this.signUpForm.value);
+  }
+
+}
