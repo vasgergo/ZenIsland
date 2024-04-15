2301fa44d0cce5f14bf65c864a2ba02f3ae01116
Vas Gergő Zoltán <h166048@stud.u-szeged.hu>
2024-04-15 13:35:36 +0200

    feat: create signup and login module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'signup', loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
