9d9d5694e04f5df1699eedf4763c8748281c5a32
Vas Gergő Zoltán <h166048@stud.u-szeged.hu>
2024-04-15 22:06:06 +0200

    feat: sidenav created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'signup', loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule) }];
+const routes: Routes = [
+  {
+    path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'signup', loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule)
+  },
+  {
+    path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,24 @@ import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 import {getStorage, provideStorage} from '@angular/fire/storage';
 import {AngularFireModule} from "@angular/fire/compat";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule,
+
+    MatSidenavModule,
+    MatToolbarModule,
     AngularFireModule.initializeApp({
       "projectId": "vasgergo-webkert",
       "appId": "1:924022631597:web:0e09a16f5544e0edcdaa24",
@@ -27,11 +37,24 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
       "messagingSenderId": "924022631597",
       "measurementId": "G-4WS8KV1PG7"
     }),
-    provideFirebaseApp(() => initializeApp({"projectId":"vasgergo-webkert","appId":"1:924022631597:web:0e09a16f5544e0edcdaa24","storageBucket":"vasgergo-webkert.appspot.com","apiKey":"AIzaSyAdeSdRm0crRW2JfUEo3BI7JAA2lzlSysE","authDomain":"vasgergo-webkert.firebaseapp.com","messagingSenderId":"924022631597","measurementId":"G-4WS8KV1PG7"})),
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "vasgergo-webkert",
+      "appId": "1:924022631597:web:0e09a16f5544e0edcdaa24",
+      "storageBucket": "vasgergo-webkert.appspot.com",
+      "apiKey": "AIzaSyAdeSdRm0crRW2JfUEo3BI7JAA2lzlSysE",
+      "authDomain": "vasgergo-webkert.firebaseapp.com",
+      "messagingSenderId": "924022631597",
+      "measurementId": "G-4WS8KV1PG7"
+    })),
     provideAuth(() => getAuth()),
     provideAnalytics(() => getAnalytics()),
     provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage())
+    provideStorage(() => getStorage()),
+    MatListItem,
+    MatNavList,
+    MatIconButton,
+    MatIcon,
+    FlexLayoutModule
   ],
   providers: [
     ScreenTrackingService,
