9d1732d61072d1ec8c3931d793b4466d86746ddd
Vas Gergő Zoltán <h166048@stud.u-szeged.hu>
2024-04-17 01:05:15 +0200

    feat: validator display

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 import {AuthService} from "../../shared/services/auth.service";
 import {User} from "../../shared/models/User";
 import {UserService} from "../../shared/services/user/user.service";
@@ -9,17 +9,33 @@ import {UserService} from "../../shared/services/user/user.service";
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
+export class SignupComponent implements OnChanges, OnInit {
 
   constructor(private authService: AuthService, private userService: UserService) {
   }
 
+  ngOnInit() {
+    this.signUpForm.statusChanges.subscribe(status => {
+        if (status === 'INVALID') {
+          console.log(this.signUpForm);
+      }
+    });
+  }
+
+  passwordMatchValidator(control:AbstractControl) : ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : {passwordMismatch: true};
+  }
+
   signUpForm = new FormGroup({
-    username: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-  });
+    username: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
+    confirmPassword: new FormControl('', [ Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
+  }, {validators: this.passwordMatchValidator});
+
+
 
   onSubmit() {
     console.log(this.signUpForm.value);
@@ -44,5 +60,9 @@ export class SignupComponent {
         console.log('AUTH: Signup failed', err);
       });
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+  };
 
 }
