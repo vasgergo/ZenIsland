0b820b0cf6cbc1950d50dd84c9fd3df322c182c2
Vas Gergő Zoltán <h166048@stud.u-szeged.hu>
2024-08-31 16:20:57 +0200

    feat: admin page appears for admin user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from "./shared/services/user/user.service";
+import {Observable} from "rxjs";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit, OnDestroy {
     title = 'vasgergo-webkert';
 
+    isAdmin$: Observable<boolean>
 
     constructor(public userService: UserService) {
-
+        this.isAdmin$ = this.userService.isAdmin();
     }
 
 
--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -2,94 +2,95 @@ import {Injectable, signal} from '@angular/core';
 import {AngularFirestore} from "@angular/fire/compat/firestore";
 import {User} from "../../models/User";
 import {Router} from "@angular/router";
-import {combineLatest, map, Observable, of, Subscription, switchMap} from "rxjs";
+import {combineLatest, delay, interval, map, Observable, of, Subscription, switchMap} from "rxjs";
 import {user} from "@angular/fire/auth";
 import {AngularFireAuth} from "@angular/fire/compat/auth";
 import {UserInterface} from "../../models/UserInterface";
 
 
 type UserIdEmail = {
-  id: string;
-  email: string;
+    id: string;
+    email: string;
 };
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
 
-  path: string = 'Users';
+    path: string = 'Users';
 
-  user$ = this.auth.user;
+    user$ = this.auth.user;
 
-  currentUserSignal = signal<UserIdEmail | null | undefined>(undefined);
+    currentUserSignal = signal<UserIdEmail | null | undefined>(undefined);
 
 
-  constructor(private afs: AngularFirestore, private router: Router, private auth: AngularFireAuth) {
-    console.log('UserService constructor');
-    this.user$.subscribe(user => {
-      if (user) {
-        this.currentUserSignal.set({
-          id: user.uid,
-          email: user.email || '',
+    constructor(private afs: AngularFirestore, private router: Router, private auth: AngularFireAuth) {
+        console.log('UserService constructor');
+        this.user$.subscribe(user => {
+            if (user) {
+                this.currentUserSignal.set({
+                    id: user.uid,
+                    email: user.email || '',
+                });
+            } else {
+                this.currentUserSignal.set(null);
+                console.log('User signed out');
+            }
+            console.log('user: ', this.currentUserSignal());
         });
-      } else {
-        this.currentUserSignal.set(null);
-        console.log('User signed out');
-      }
-      console.log('user: ', this.currentUserSignal());
-    });
-  }
+    }
 
-  signin(email: string, password: string) {
-    return this.auth.signInWithEmailAndPassword(email, password);
-  }
+    signin(email: string, password: string) {
+        return this.auth.signInWithEmailAndPassword(email, password);
+    }
 
-  signup(email: string, password: string) {
-    return this.auth.createUserWithEmailAndPassword(email, password);
-  }
+    signup(email: string, password: string) {
+        return this.auth.createUserWithEmailAndPassword(email, password);
+    }
 
-  signout(): void {
-    this.auth.signOut().then(() => {
-      console.log('signout success');
-    });
-    this.router.navigate(['/signin']);
-  }
+    signout(): void {
+        this.auth.signOut().then(() => {
+            console.log('signout success');
+        });
+        this.router.navigate(['/signin']);
+    }
 
-  getSignedInUserId(): string | null {
-    return this.currentUserSignal()?.id || null;
-  }
+    getSignedInUserId(): string | null {
+        return this.currentUserSignal()?.id || null;
+    }
 
-  isSignedIn(): boolean {
-    return this.currentUserSignal() !== null && this.currentUserSignal() !== undefined;
-  }
+    isSignedIn(): boolean {
+        return this.currentUserSignal() !== null && this.currentUserSignal() !== undefined;
+    }
 
-  create(user: User) {
-    return this.afs.collection<User>(this.path).add(user);
-  }
+    create(user: User) {
+        return this.afs.collection<User>(this.path).add(user);
+    }
 
-  isSignedInObs(): Observable<boolean> {
-    return this.user$.pipe(
-      map(user => user !== null)
-    );
-  }
+    isSignedInObs(): Observable<boolean> {
+        return this.user$.pipe(
+            map(user => user !== null)
+        );
+    }
 
 
-  isAdmin(): Observable<boolean> {
-    return this.isSignedInObs().pipe(
-      switchMap(isSignedIn => {
-        if (!isSignedIn) {
-          return of(false);
-        }
-        return this.afs.collection<User>(this.path, user => user.where('id', '==', this.currentUserSignal()?.id))
-          .valueChanges()
-          .pipe(
-            map(users => users.length > 0 && users[0].admin)
-          );
-      })
-    );
-  }
+
+    isAdmin(): Observable<boolean> {
+        return this.isSignedInObs().pipe(
+            switchMap(isSignedIn => {
+                if (!isSignedIn) {
+                    return of(false);
+                }
+                return this.afs.collection<User>(this.path, user => user.where('id', '==', this.currentUserSignal()?.id))
+                    .valueChanges()
+                    .pipe(
+                        map(users => users.length > 0 && users[0].admin)
+                    );
+            })
+        );
+    }
 
 
 }
